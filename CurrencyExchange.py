503a5f9bfc3eb23109d4c0d0c7b993d06b78c404
PSD-Alex <aleabrahamsen@hotmail.com>
2020-03-09 10:56:20 +0100

    V 1.4 (New currency)

--- CurrencyExchange.py
+++ CurrencyExchange.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[4]:
 
 
-# V 1.3
+# V 1.4
 def exchange(nok_amount):
     nok_gbp = 12.56
     nok_usd = 9.55
+    nok_sek = 1.01
     
     gbp_result = nok_amount / nok_gbp
     usd_result = nok_amount / nok_usd
+    sek_result = nok_amount / nok_sek
     
     print("GBP: ", gbp_result)
     print("USD: ", usd_result)
+    print("SEK: ", sek_result)
     
 exchange(100)
 
